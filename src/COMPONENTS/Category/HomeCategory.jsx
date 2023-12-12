13ea255e198c164fcad3518757dbab92a928c394
Nithin Prathapan <nithinprathapan369@gmail.com>
2023-12-12 07:52:35 +0530

    category

---
+++ src/COMPONENTS/Category/HomeCategory.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./HomeCategory.css";
+import clip1 from "../../Assets/images/clip1.png";
+import clip2 from "../../Assets/images/clip2.png";
+import clip3 from "../../Assets/images/clip3.png";
+import clip4 from "../../Assets/images/clip4.png";
+
+const HomeCategory = () => {
+  return (
+    <div className="home-category">
+      <div className="container">
+        <img src={clip1} alt="noimage" />
+        <div className="content">
+          <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+      </div>
+      <div className="container">
+        <img src={clip2} alt="noimage" />
+        <div className="content">
+          <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+      </div>
+      <div className="container">
+        <img src={clip3} alt="noimage" />
+        <div className="content">
+          <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+      </div>
+      <div className="container">
+        <img src={clip4} alt="noimage" />
+        <div className="content">
+          <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeCategory;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../COMPONENTS/Navbar/Navbar";
 import BannerSlider from "../COMPONENTS/Banners/BannerSlider";
+import HomeCategory from "../COMPONENTS/Category/HomeCategory";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <BannerSlider />
+      <HomeCategory />
     </div>
   );
 };
